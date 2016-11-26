cf14986625c2de5d6e6de2418579dadf02c9bbe7
KIDJourney <kingdeadfish@qq.com>
2016-11-26 18:18:31 +0800

    restruct

---
+++ board/serializers.py
@@ -0,0 +1,19 @@
+from .models import Events
+from rest_framework import serializers
+
+
+class EventSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, allow_blank=False, max_length=20)
+    content = serializers.CharField(required=True, allow_blank=True, max_length=65535)
+    event_type = serializers.ChoiceField(choices=[0, 1], required=True, allow_blank=False)
+
+    def create(self, validated_data):
+        return Events.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+
+        instance.save()
+        return instance
