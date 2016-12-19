b72cf15a483408289d2b1c4d043c696afbbf7872
KIDJourney <kingdeadfish@qq.com>
2016-12-19 14:52:20 +0800

    finish order

--- brb/mixins.py
+++ brb/mixins.py
@@ -3,10 +3,22 @@ class ViewSearchMixin(object):
     object_model = None
 
     def filter_queryset(self, queryset):
+        order = self.request.query_params.get('order', 'asc')
+        orderby = self.request.query_params.get('orderby', 'created_at')
+
         for attr, value in self.request.query_params.items():
             if hasattr(self.object_model, attr):
                 try:
                     queryset = queryset.filter(**{attr: value})
+                except Exception:
+                    return []
+
+        if order and orderby:
+            if hasattr(self.object_model, orderby):
+                if order == 'desc':
+                    orderby = '-' + orderby
+                try:
+                    queryset = queryset.order_by(orderby)
                 except Exception:
                     return []
 
